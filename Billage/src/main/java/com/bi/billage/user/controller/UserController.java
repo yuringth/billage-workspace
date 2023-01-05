74073971ec3fa785e188ebcf9d2449ce6b0cbc86
djflsdl8 <djflsdl8@naver.com>
2023-01-05 16:03:38 +0900

    관리자 사이드바 등 -광진

--- Billage/src/main/java/com/bi/billage/user/controller/UserController.java
+++ Billage/src/main/java/com/bi/billage/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.bi.billage.user.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class UserController {
 	
 	@RequestMapping("admin.ad")
