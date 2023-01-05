22d030e1b135e192fbba873be827cc36cd2b6e36
parkSeunggeun <121650324+parkSeunggeun@users.noreply.github.com>
2023-01-05 15:50:10 +0900

    uservo생성, usercontroller -수정
    UserVo에 필드 생성
    UserController에 추가

--- Billage/src/main/java/com/bi/billage/user/controller/UserController.java
+++ Billage/src/main/java/com/bi/billage/user/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.bi.billage.user.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class UserController {
 	
 	@RequestMapping("admin.ad")
 	public String adminPage() {
 		return "admin/adminPage";
+	}
+	
+	@RequestMapping("userEnrollForm.me")
+	public String userEnrollForm() {
+		return "user/userEnrollForm";
 	}
 }
