b5d7832c40b37d7f9d4fff480c1ecf3ca5afea3b
mesutkim <110654879+rlatpgjs2@users.noreply.github.com>
2023-01-06 13:26:43 +0900

    Update MessageController.java

--- Billage/src/main/java/com/bi/billage/message/controller/MessageController.java
+++ Billage/src/main/java/com/bi/billage/message/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.bi.billage.message.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MessageController {
