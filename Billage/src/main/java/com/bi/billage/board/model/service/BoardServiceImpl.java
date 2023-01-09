ec82146621e5669ad285ef0b7a3ebf4428f54ca1
mesutkim <110654879+rlatpgjs2@users.noreply.github.com>
2023-01-09 10:51:00 +0900

    IMPL 꽉잡아!! -세헌

--- Billage/src/main/java/com/bi/billage/board/model/service/BoardServiceImpl.java
+++ Billage/src/main/java/com/bi/billage/board/model/service/BoardServiceImpl.java
@@ -1,14 +1,85 @@
 package com.bi.billage.board.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
+
+import com.bi.billage.board.model.vo.ADBoard;
 
 @Service
 public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public int insertReport() {
-		
+		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public int increaseCount(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<ADBoard> selectDrawBoardList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertDrawBoard(ADBoard b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ADBoard selectDrawBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteDrawBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateDrawBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<ADBoard> selectAuctionBoardList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertAuctionBoard(ADBoard b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ADBoard selectAuctionBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deleteAuctionBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateAuctionBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 
 }
