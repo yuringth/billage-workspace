b467ffb6db339f7b1cfcf1339c19b31717d921df
yuringth <121650385+yuringth@users.noreply.github.com>
2023-01-09 14:27:46 +0900

    boardDao 커밋-유림

--- Billage/src/main/java/com/bi/billage/board/model/dao/BoardDao.java
+++ Billage/src/main/java/com/bi/billage/board/model/dao/BoardDao.java
@@ -147,7 +147,7 @@ public class BoardDao {
 	
 	// 유림시작
 	public int insertUsedBoard(SqlSessionTemplate sqlSession, UsedBoard b) {
-		return sqlSession.insert("usedMapper.insertUsedBoard");	
+		return sqlSession.insert("usedMapper.insertUsedBoard", b);	
 		
 	}
 
