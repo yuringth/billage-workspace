0ff3b4ce42a632b0f7ae0a6bdab361766a3fb376
hwwizzang <121650365+khs1018@users.noreply.github.com>
2023-01-09 16:05:43 +0900

    ....수정 -휘수-

--- Billage/src/main/java/com/bi/billage/user/model/dao/UserDao.java
+++ Billage/src/main/java/com/bi/billage/user/model/dao/UserDao.java
@@ -15,7 +15,6 @@ public class UserDao {
 	
 	// 로그인
 	public User loginUser(SqlSessionTemplate sqlSession, User u) {
-		System.out.println(u);
 		return sqlSession.selectOne("userMapper.loginUser", u);
 	}
 }
