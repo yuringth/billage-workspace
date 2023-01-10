0e72cf278ebc4b035baecf9423d73cc0f629759c
maerany <88430300+maerany@users.noreply.github.com>
2023-01-10 17:07:30 +0900

    -매란-

--- Billage/src/main/java/com/bi/billage/group/model/service/GroupServiceImpl.java
+++ Billage/src/main/java/com/bi/billage/group/model/service/GroupServiceImpl.java
@@ -2,12 +2,13 @@ package com.bi.billage.group.model.service;
 
 import org.springframework.stereotype.Service;
 
+import com.bi.billage.group.model.dao.GroupDao;
 import com.bi.billage.group.model.vo.Group;
 
 @Service
 public class GroupServiceImpl implements GroupService {
 
-	
+	private GroupDao groupDao;
 	
 	@Override
 	public int insertGroup(Group group) {
