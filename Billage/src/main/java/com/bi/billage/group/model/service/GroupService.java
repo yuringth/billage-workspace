19dd55c2bc960a0f615dae0758d8d776912a8ba4
maerany <88430300+maerany@users.noreply.github.com>
2023-01-10 23:48:07 +0900

    커밋 중간

--- Billage/src/main/java/com/bi/billage/group/model/service/GroupService.java
+++ Billage/src/main/java/com/bi/billage/group/model/service/GroupService.java
@@ -1,8 +1,15 @@
 package com.bi.billage.group.model.service;
 
+import java.util.ArrayList;
+
+import com.bi.billage.common.model.vo.PageInfo;
 import com.bi.billage.group.model.vo.Group;
 
 public interface GroupService {
 	
-	public int insertGroup(Group group);
+	int selectListCount();
+	
+	ArrayList<Group> selectList(PageInfo pi);
+	
+	int insertGroup(Group group);
 }
--- Billage/src/main/java/com/bi/billage/group/model/service/GroupServiceImpl.java
+++ Billage/src/main/java/com/bi/billage/group/model/service/GroupServiceImpl.java
@@ -1,21 +1,42 @@
 package com.bi.billage.group.model.service;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bi.billage.common.model.vo.PageInfo;
 import com.bi.billage.group.model.dao.GroupDao;
 import com.bi.billage.group.model.vo.Group;
 
 @Service
 public class GroupServiceImpl implements GroupService {
-
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
 	private GroupDao groupDao;
 	
 	@Override
-	public int insertGroup(Group group) {
-		
-	
-		return 0;
+	public int selectListCount() {
+		return groupDao.selectListCount(sqlSession);
 	}
+	
+	@Override
+	public ArrayList<Group> selectList(PageInfo pi) {
+		return groupDao.selectList(sqlSession, pi);
+	}
+		
+	@Override
+	public int insertGroup(Group group) {
+		return groupDao.insertGroup(sqlSession, group);
+	}
+
+
+
+
 
 	
 }
