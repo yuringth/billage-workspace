6b30a7799cd5fabbcc9482971ad22a2196819c5b
mesutkim <110654879+rlatpgjs2@users.noreply.github.com>
2023-01-11 12:14:02 +0900

    잠시만

--- Billage/src/main/java/com/bi/billage/board/controller/DrawAuctionController.java
+++ Billage/src/main/java/com/bi/billage/board/controller/DrawAuctionController.java
@@ -125,7 +125,7 @@ public class DrawAuctionController {
 	@RequestMapping("detail.ac")
 	public ModelAndView auctionDetailView(int bno, ModelAndView mv) {
 		if(boardService.auctionIncreaseCount(bno) > 0) {
-			mv.addObject("b", boardService.selectAuctionBoard(bno)).setViewName("board/drawBoard/auctionDetailView");
+			mv.addObject("b", boardService.selectAuctionBoard(bno)).setViewName("board/auctionBoard/auctionDetailView");
 		} else {
 			mv.addObject("errorMsg", "게시글 조회 실패").setViewName("common/errorPage");
 		}
