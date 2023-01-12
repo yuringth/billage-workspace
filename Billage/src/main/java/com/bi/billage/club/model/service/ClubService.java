f1a1304a472f188c81f68123fb944ff01b5cf03b
maerany <88430300+maerany@users.noreply.github.com>
2023-01-12 16:54:21 +0900

    하핫

--- Billage/src/main/java/com/bi/billage/club/model/service/ClubService.java
+++ Billage/src/main/java/com/bi/billage/club/model/service/ClubService.java
@@ -1,24 +1,24 @@
-package com.bi.billage.group.model.service;
+package com.bi.billage.club.model.service;
 
 import java.util.ArrayList;
 
+import com.bi.billage.club.model.vo.Club;
 import com.bi.billage.common.model.vo.PageInfo;
-import com.bi.billage.group.model.vo.Group;
 
-public interface GroupService {
+public interface ClubService {
 	
 	// Group 게시글의 List 총 개수 조회
 	int selectListCount();
 	
 	// 페이징 처리
-	ArrayList<Group> selectList(PageInfo pi);
+	ArrayList<Club> selectList(PageInfo pi);
 	
 	// 게시글 클릭 시 조회수 증가
 	int increaseCount(int groupNo);
 	
 	// 게시글 조회수 증가 후 해당 게시글의 상세정보 조회 
-	Group selectDetailGroup(int groupNo);
+	Club selectDetailGroup(int groupNo);
 	
 	// 모임 insert 
-	int insertGroup(Group group);
+	int insertGroup(Club group);
 }
